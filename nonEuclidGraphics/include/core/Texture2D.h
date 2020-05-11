5144199f01e669f2ee5185957e256a87438f2370
EasternJournalist <47034050+EasternJournalist@users.noreply.github.com>
2020-05-12 01:16:07 +0800

    更新Texture2D
    texture2d能通过SetTexutreImage绑定到gl。需要使用时用Bind()，结束用BindReset()。
    
    问题：还没有清空缓存

--- nonEuclidGraphics/include/core/Texture2D.h
+++ nonEuclidGraphics/include/core/Texture2D.h
@@ -5,9 +5,10 @@ openCV: 修改工程的包含目录、库目录、附加依赖项
 
 #include <core/vec.h>
 #include <core/mat.h>
-#include <core/Mesh.h>
 #include <core/transform.h>
 
+#include <GL/gl3w.h>            // Initialize with gl3wInit()
+#include <GLFW/glfw3.h>
 #include <opencv2/opencv.hpp>
 
 namespace cgcore
@@ -23,62 +24,26 @@ namespace cgcore
 		vecf3 Sample(vecf2 uv);
 
 	private:
-
+		std::string image_path;
 
 	public:
 		enum class WrapMode { Clamp, Repeat, Mirror };
 		enum class SampleMode { Nearest, Linear };
 
 		cv::Mat img;
+		GLuint id = 0;					//OpenGL用的id
+
+		//生成OpenGL的纹理
+		void SetTextureImage();
+
+		void Bind();
+		void BindReset();
+
+		bool IsValid();
 
 	private:
 
 	};
 
-	Texture2D::Texture2D()
-	{
-	}
 
-	inline Texture2D::Texture2D(const std::string& path)
-	{
-		Load(path);
-	}
-
-	Texture2D::~Texture2D()
-	{
-	}
-
-	inline void Texture2D::Load(const std::string& path)
-	{
-		img = cv::imread(path, cv::IMREAD_COLOR);//3 channel BGR
-		if (img.empty())
-		{
-			std::cout << "ERROR::Texture2D::Load:" << std::endl
-				<< "\t" << "open file (" << path << ") fail" << std::endl;
-		}
-	}
-
-	vecf3 Texture2D::Sample(vecf2 uv)//
-	{
-		if (img.empty())
-		{
-			std::cout << "ERROR::Texture2D::Sample:" << std::endl
-				<< "\t" << "img is empty" << std::endl;
-			return vecf3({ 0, 1, 0 });
-		}
-
-		//repeat mode
-		uv[0] -= int(uv[0]) / 1.0;
-		uv[1] -= int(uv[1]) / 1.0;
-
-		int xidx = int(uv[0] * img.cols);
-		int yidx = int(uv[1] * img.rows);
-		float xlambda = uv[0] * img.cols - xidx;
-		float ylambda = uv[1] * img.rows - yidx;
-		//双线性插值
-		cv::Vec3b result = (1 - ylambda) * ((1 - xlambda) * img.at<cv::Vec3b>(yidx, xidx) + xlambda * img.at<cv::Vec3b>(yidx, (xidx + 1) % img.cols))
-			+ ylambda * ((1 - xlambda) * img.at<cv::Vec3b>((yidx + 1) % img.rows, xidx) + xlambda * img.at<cv::Vec3b>((yidx + 1) % img.rows, (xidx + 1) % img.cols));
-
-		return vecf3({(float)result[2], (float)result[1] , (float)result[0] });//转换为RGB
-	}
 }
---
+++ nonEuclidGraphics/src/core/Texture2D.cpp
@@ -0,0 +1,92 @@
+
+#include <core/Texture2D.h>
+
+using namespace cgcore;
+
+Texture2D::Texture2D()
+{
+}
+
+Texture2D::Texture2D(const std::string& path)
+{
+	Load(path);
+	image_path = path;
+}
+
+Texture2D::~Texture2D()
+{
+}
+
+void Texture2D::Load(const std::string& path)
+{
+	img = cv::imread(path, cv::IMREAD_COLOR);//3 channel BGR
+	if (img.empty())
+	{
+		std::cout << "ERROR::Texture2D::Load:" << std::endl
+			<< "\t" << "open file (" << path << ") fail" << std::endl;
+	}
+	image_path = path;
+}
+
+vecf3 Texture2D::Sample(vecf2 uv)
+{
+	if (img.empty())
+	{
+		std::cout << "ERROR::Texture2D::Sample:" << std::endl
+			<< "\t" << "img is empty" << std::endl;
+		return vecf3({ 0, 1, 0 });
+	}
+	//repeat mode
+	uv[0] -= float(int(uv[0]));
+	uv[1] -= float(int(uv[1]));
+
+	int xidx = int(uv[0] * img.cols);
+	int yidx = int(uv[1] * img.rows);
+	float xlambda = uv[0] * img.cols - xidx;
+	float ylambda = uv[1] * img.rows - yidx;
+	//双线性插值
+	cv::Vec3b result = (1 - ylambda) * ((1 - xlambda) * img.at<cv::Vec3b>(yidx, xidx) + xlambda * img.at<cv::Vec3b>(yidx, (xidx + 1) % img.cols))
+		+ ylambda * ((1 - xlambda) * img.at<cv::Vec3b>((yidx + 1) % img.rows, xidx) + xlambda * img.at<cv::Vec3b>((yidx + 1) % img.rows, (xidx + 1) % img.cols));
+
+	return vecf3({ (float)result[2], (float)result[1] , (float)result[0] });//转换为RGB
+}
+
+void cgcore::Texture2D::SetTextureImage()
+{
+	if (img.empty())
+	{
+		std::cout << "Texture2D::SetTextureImage : No available texture image" << std::endl;
+		return;
+	}
+	GLsizei width = img.cols, height = img.rows;
+	//生成id并绑定
+	glGenTextures(1, &id);					
+	glBindTexture(GL_TEXTURE_2D, id);
+
+	//设置纹理参数
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	//把纹理数据传给OpenGL
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, img.data);	
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	BindReset();
+}
+
+void cgcore::Texture2D::Bind()
+{
+	glBindTexture(GL_TEXTURE_2D, id);
+}
+
+void cgcore::Texture2D::BindReset()
+{
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+bool cgcore::Texture2D::IsValid()
+{
+	return id != 0;
+}
