f07b98e12f1681eda1cfe95344c50516e550bbed
方奕然 <fyr233@mail.ustc.edu.cn>
2020-05-11 08:34:12 +0800

    Update Texture2D.h
    使用openCV，写了双线性插值

--- nonEuclidGraphics/include/core/Texture2D.h
+++ nonEuclidGraphics/include/core/Texture2D.h
@@ -1,9 +1,14 @@
 #pragma once
+/*
+openCV: 修改工程的包含目录、库目录、附加依赖项
+*/
 
 #include <core/vec.h>
 #include <core/mat.h>
 #include <core/Mesh.h>
 #include <core/transform.h>
+
+#include <opencv2/opencv.hpp>
 
 namespace cgcore
 {
@@ -24,7 +29,7 @@ namespace cgcore
 		enum class WrapMode { Clamp, Repeat, Mirror };
 		enum class SampleMode { Nearest, Linear };
 
-		//Image image;
+		cv::Mat img;
 
 	private:
 
@@ -45,12 +50,31 @@ namespace cgcore
 
 	inline void Texture2D::Load(const std::string& path)
 	{
-		//TODO
+		img = cv::imread(path, cv::IMREAD_COLOR);//3 channel BGR
+		if (img.empty())
+		{
+			std::cout << "ERROR::Texture2D::Load:" << std::endl
+				<< "\t" << "open file (" << path << ") fail" << std::endl;
+		}
 	}
 
-	inline vecf3 Texture2D::Sample(vecf2 uv)
+	vecf3 Texture2D::Sample(vecf2 uv)//u, v in [0,1)
 	{
-		//TODO
-		return vecf3();
+		if (img.empty())
+		{
+			std::cout << "ERROR::Texture2D::Sample:" << std::endl
+				<< "\t" << "img is empty" << std::endl;
+			return vecf3({ 0, 1, 0 });
+		}
+
+		int xidx = int(uv[0] * img.cols);
+		int yidx = int(uv[1] * img.rows);
+		float xlambda = uv[0] * img.cols - xidx;
+		float ylambda = uv[1] * img.rows - yidx;
+		//双线性插值
+		cv::Vec3b result = (1 - ylambda) * ((1 - xlambda) * img.at<cv::Vec3b>(yidx, xidx) + xlambda * img.at<cv::Vec3b>(yidx, (xidx + 1) % img.cols))
+			+ ylambda * ((1 - xlambda) * img.at<cv::Vec3b>((yidx + 1) % img.rows, xidx) + xlambda * img.at<cv::Vec3b>((yidx + 1) % img.rows, (xidx + 1) % img.cols))；
+
+		return vecf3({result[2], result[1] , result[0] });//转换为RGB
 	}
 }
