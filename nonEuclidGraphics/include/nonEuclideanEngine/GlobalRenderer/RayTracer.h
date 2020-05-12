befd512b670bfbf26b95f5cc71d7f25a334b22c5
EasternJournalist <47034050+EasternJournalist@users.noreply.github.com>
2020-05-12 17:11:52 +0800

    RayTracer TODO

---
+++ nonEuclidGraphics/include/nonEuclideanEngine/GlobalRenderer/RayTracer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+//FAST-BVH
+#include <FastBVH.h>
+
+#include <core/rgb.h>
+#include <nonEuclideanEngine/world.h>
+
+#include <core/primitive.h>
+namespace nonEuc
+{
+
+	class RayTracer
+	{
+	public:
+		World& world;
+		
+		FastBVH::BVH<float, Triangle> bvh;
+
+		std::vector<Triangle> triangles;		//我们的所有 primitive 都是 Triangle。world中的所有obj/area/light的三角形都统一放到triangles中
+
+		float decay;							//衰减系数
+
+	private:
+		
+		// 生成world中的所有三角形，生成BVH
+		void BuildBVH();
+
+		//求解一条光线的着色器
+		//ray:射线及其方向, distance:光线已走距离（距离发出为止）, times 光线反射的次数
+		rgbf shader(FastBVH::Ray<float> ray, float distance, int times);
+	};
+}
---
+++ nonEuclidGraphics/src/nonEuclideanEngine/GlobalRenderer/RayTracer.cpp
@@ -0,0 +1,29 @@
+
+#include <nonEuclideanEngine/GlobalRenderer/RayTracer.h>
+
+void nonEuc::RayTracer::BuildBVH()
+{
+	//triangles = world.GetTriangles();			//TODO
+
+	FastBVH::DefaultBuilder<float> builder;
+
+	auto boxConverter = [](const Triangle& t) {
+		FastBVH::Vector3<float> minbound, maxbound;
+		minbound[0] = std::min(std::min(t.pos[0][0], t.pos[1][0]), t.pos[2][0]);
+		minbound[1] = std::min(std::min(t.pos[0][1], t.pos[1][1]), t.pos[2][1]);
+		minbound[2] = std::min(std::min(t.pos[0][2], t.pos[1][2]), t.pos[2][2]);
+
+		minbound[0] = std::max(std::max(t.pos[0][0], t.pos[1][0]), t.pos[2][0]);
+		minbound[1] = std::max(std::max(t.pos[0][1], t.pos[1][1]), t.pos[2][1]);
+		minbound[2] = std::max(std::max(t.pos[0][2], t.pos[1][2]), t.pos[2][2]);
+
+		return FastBVH::BBox<float>(minbound, maxbound);
+	};
+	bvh = builder(triangles, boxConverter);
+}
+
+rgbf nonEuc::RayTracer::shader(FastBVH::Ray<float> ray, float distance, int times)
+{
+	
+	
+}
