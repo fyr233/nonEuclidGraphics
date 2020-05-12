decd9f2b6070bfee7e0666b0285a686de1e72a7b
EasternJournalist <47034050+EasternJournalist@users.noreply.github.com>
2020-05-13 02:02:53 +0800

    AreaLight和rgb

--- nonEuclidGraphics/include/core/AreaLight.h
+++ nonEuclidGraphics/include/core/AreaLight.h
@@ -15,14 +15,14 @@ namespace cgcore
 	{
 	public:
 		//AreaLight();
-		AreaLight(float intensity = 1.f, const rgbf& color = rgbf{ 1.f }, Texture2D* texture = nullptr);
+		AreaLight(nonEuc::World* _world, float intensity = 1.f, const rgbf& color = rgbf{ 1.f }, Texture2D* texture = nullptr);
 			
 		~AreaLight();
 
 		rgbf RadianceFactor() { return  color * intensity; }
 		rgbf Radiance(const vecf2& uv) ;
 
-		virtual void Draw(GLuint programID);
+		void Draw(GLuint programID);
 
 	private:
 
@@ -35,37 +35,4 @@ namespace cgcore
 		rgbf color;	//rgb
 		Texture2D* texture = nullptr;
 	};
-
-	AreaLight::AreaLight(float intensity, const rgbf& color, Texture2D* texture)
-		: intensity{ intensity }, color{ color }, texture{ texture }
-	{
-		tri.pos[0] = vecf3({ 0, 0, 0 });
-		tri.pos[1] = vecf3({ 1, 0, 0 });
-		tri.pos[2] = vecf3({ 0, 1, 0 });
-		tri.idx[0] = 0;
-		tri.idx[1] = 1;
-		tri.idx[2] = 2;
-		tri.obj_id = this->obj_id;
-
-		this->obj_type = _AreaLight;
-	}
-
-	AreaLight::~AreaLight()
-	{
-	}
-
-	inline rgbf AreaLight::Radiance(const vecf2& uv)
-	{
-		if (texture != nullptr)
-		{
-			return texture->Sample(uv) * intensity;
-		}
-		else
-		{
-			return color * intensity;//Ã»ÓÐtexture
-		}
-	}
-	inline void AreaLight::Draw(GLuint programID)
-	{
-	}
 }
--- nonEuclidGraphics/include/core/rgb.h
+++ nonEuclidGraphics/include/core/rgb.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <initializer_list>
+#include <assert.h>
 
 typedef unsigned char uchar;
 
